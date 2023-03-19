9bbce7fdef6773f781e1813422137f1fba11af84
Sir-Floppa <nicolas.donoso.ge@outlook.com>
2023-03-19 12:41:40 -0300

    finished form styles

--- src/app/card-form/card-form.component.ts
+++ src/app/card-form/card-form.component.ts
@@ -3,8 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-card-form',
   templateUrl: './card-form.component.html',
-  styleUrls: ['./card-form.component.scss']
+  styleUrls: ['./card-form.component.scss'],
 })
 export class CardFormComponent {
+  public formatCardNumber = (e: Event) => {
+    const creditCardRegex: RegExp = /\d{4}/g;
+    const nonNumberRegex: RegExp = /[a-zA-Z]/g;
+  };
 
+  public cardNumber: String;
 }
