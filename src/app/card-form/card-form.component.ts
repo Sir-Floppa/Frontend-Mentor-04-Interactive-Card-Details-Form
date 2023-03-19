862da5216302d9374c25764b1a99b3b360294995
Sir-Floppa <nicolas.donoso.ge@outlook.com>
2023-03-19 12:01:53 -0300

    added form

---
+++ src/app/card-form/card-form.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-card-form',
+  templateUrl: './card-form.component.html',
+  styleUrls: ['./card-form.component.scss']
+})
+export class CardFormComponent {
+
+}
