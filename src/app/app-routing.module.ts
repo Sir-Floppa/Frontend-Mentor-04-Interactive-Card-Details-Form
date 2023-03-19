6287cc3ba4564f561e2482cbcd1061e06d515dcc
Sir-Floppa <nicolas.donoso.ge@outlook.com>
2023-03-19 20:28:35 -0300

    added basic routing config

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CardFormComponent } from './card-form/card-form.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', pathMatch: "full", redirectTo: 'card-form'},
+  {path: '', children: [
+    {path: 'card-form', component: CardFormComponent}
+  ]}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
