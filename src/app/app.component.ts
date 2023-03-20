2c58ec1fc69aae982bdda81d3e87c16f3e40d920
Sir-Floppa <nicolas.donoso.ge@outlook.com>
2023-03-20 17:20:27 -0300

    added card initializacion on app-component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,8 +10,20 @@ import { CardDataService } from './card-data.service';
 export class AppComponent {
 
   constructor(private cardData: CardDataService) { 
-    cardData.changeEmitted$.subscribe(value => this.card = value)
+    cardData.changeEmitted$.subscribe(value => {
+      this.card.cardholder = value.cardholder ? value.cardholder: this.card.cardholder;
+      this.card.cardNumber = value.cardNumber ? value.cardNumber: this.card.cardNumber;
+      this.card.expMonth = value.expMonth ? value.expMonth: this.card.expMonth;
+      this.card.expYear = value.expYear ? value.expYear: this.card.expYear;
+      this.card.cvc = value.cvc ? value.cvc: this.card.cvc;
+    })
   }
 
-  card: Card;
+  card: Card = {
+    cardholder: 'Jane Appleseed',
+    cardNumber: '0000 0000 0000 0000',
+    expMonth: '00',
+    expYear: '00',
+    cvc: '123'
+  };
 }
